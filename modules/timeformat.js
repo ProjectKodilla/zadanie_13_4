6930f314f9291c4a5098edde9ea7c51415a74da8
Marcin Bogdanski <marcin@bogdanski.info>
2018-04-07 11:25:15 +0200

    finished

--- modules/timeformat.js
+++
@@ -1,8 +0,0 @@
-function time (sec) {
-  var hour = Math.floor(sec / 3600)
-  var minute = Math.floor(sec % 3600 / 60)
-  var second = (sec % 60).toFixed(0)
-  return hour + 'h ' + minute + 'min ' + second + 's ';
-}
-
-exports.print = time
